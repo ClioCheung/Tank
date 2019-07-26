46dcf5f399c2e1a57949faa98f60fd67fac980fa
Clio <cliostill1@gmail.com>
2019-07-26 10:23:27 +0800

    Server端保留Client端信息

--- src/project_02_TankWar/NetClient.java
+++ src/project_02_TankWar/NetClient.java
@@ -1,19 +1,42 @@
 package project_02_TankWar;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class NetClient {
+	private static int UDP_PORT_START = 2223;
+	private int udpPort;
+	
+	public NetClient() {
+		this.udpPort = UDP_PORT_START ++;
+	}
+	
 	
 	public void connected(String IP,int port) {
+		Socket socket = null;
 		try {
-			Socket socket = new Socket(IP,port);
+			socket = new Socket(IP,port);
 System.out.println("connected to server!");
+
+			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+			dos.writeInt(port);
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			if(socket != null) {
+				try {
+					socket.close();
+					socket = null;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
 		}
 	
 	}
--- src/project_02_TankWar/TankServer.java
+++ src/project_02_TankWar/TankServer.java
@@ -1,23 +1,66 @@
 package project_02_TankWar;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TankServer {
 	public static final int TCP_PORT = 6666;
-			
+	List<Client> clients = new ArrayList<Client>();
+		
+	
 	public static void main(String[] args) {
+		new TankServer().start();
+	}
+	
+	
+	public void start() {
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		
 		try {
-			ServerSocket serverSocket = new ServerSocket(TCP_PORT);
+			serverSocket = new ServerSocket(TCP_PORT);
 			while(true) {
-				Socket socket = serverSocket.accept();
+				socket = serverSocket.accept();
 System.out.println("A Client connected! Addr- " + socket.getInetAddress() + ":" + socket.getPort());
+				
+				DataInputStream dis = new DataInputStream(socket.getInputStream());
+				String IP = socket.getInetAddress().getHostAddress();
+				int udpPort = dis.readInt();
+				
+				Client c = new Client(IP,udpPort);
+				clients.add(c);
 			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			if (socket != null) {
+				try {
+					socket.close();
+					socket = null;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
 		}
-
+		
+	}
+	
+	
+	private class Client {
+		String IP;
+		int udpPort;
+				
+		public Client(String IP,int udpPort) {
+			this.IP = IP;
+			this.udpPort = udpPort;
+		}
+				
 	}
 
 }
